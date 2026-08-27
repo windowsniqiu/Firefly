f7a6e11bd5ccf1676f35b7dd58941fdc071e0488
windowsniqiu <HelloGoldenLoach@outlook.com>
2026-08-27 12:43:35 +0800

    Modify links in profileConfig
    Updated GitHub and Email URLs in profileConfig.

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -22,27 +22,15 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "qq",
-			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
-			showName: false,
-		},
-		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/windowsniqiu",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
-			showName: false,
-		},
-		{
-			name: "RSS",
-			icon: "fa7-solid:rss",
-			url: "/rss/",
+			url: "mailto:niqiu@kipfelemail.kdns.fr",
 			showName: false,
 		},
 	],
