f20e93d263d2ce89c04cc61d1fc1a21cf8d33a2f
windowsniqiu <HelloGoldenLoach@outlook.com>
2026-08-27 20:10:18 +0800

    Update sponsorConfig.ts

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -25,7 +25,7 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "微信",
 			icon: "fa7-brands:weixin",
-			qrCode: "/assets/images/sponsor/wechat.png",
+			qrCode: "/assets/images/sponsor/1.png",
 			link: "",
 			description: "使用 微信 扫码打赏",
 			enabled: true,
