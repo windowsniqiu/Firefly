2a7e79fbd17ede6afbdf75b9885b0211049a464b
windowsniqiu <HelloGoldenLoach@outlook.com>
2026-08-28 15:13:19 +0800

    Update musicConfig.ts

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -50,8 +50,8 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 			{
 				name: "使一颗心免于哀伤",
 				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				url: "/assets/music/summer.mp3",
+				cover: "/assets/music/cover/summer.jpg",
 				lrc: "",
 			},
 		],
